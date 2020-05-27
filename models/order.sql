1badde49a97e4ddd4c3127bf1215bddc3c43b4c4
Rajan Patel <rapatel@fastly.com>
2020-05-27 18:38:19 +0000

    adding orders

---
+++ models/order.sql
@@ -0,0 +1,10 @@
+select 
+o."ID" as orderid
+, c."ID" as customerid
+, sum(p."AMOUNT") as totalpayment
+FROM RAW.JAFFLE_SHOP.ORDERS o 
+JOIN RAW.JAFFLE_SHOP.CUSTOMERS c
+ ON o."USER_ID" = c."ID"
+JOIN RAW.STRIPE.PAYMENT p
+ON o."ID" = p."orderID"
+group by 2,1
